81fd74d1de1cb2a56126e5902d71b3874cb12e88
Jukka Svahn <void@rah.pw>
2019-10-12 21:51:42 +0300

    Try building into the workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,27 @@
 #!/usr/bin/env bash
 set -eu
 
-if ! [ "${INPUT_NAME:-}" ]; then
-  INPUT_NAME="default"
-fi
+artifacts="${GITHUB_WORKSPACE:-/build}/packages/${INPUT_NAME:-default}"
 
 mkdir -p /packages/
 
-mkdir -p "$HOME/packages/$INPUT_NAME"
+mkdir -p "$artifacts"
 
 compile "${GITHUB_WORKSPACE:-src}/${INPUT_SOURCE:-}" /packages/ > /dev/null
 
-compressed="$HOME/packages/$INPUT_NAME/compressed"
-
 for f in "/packages/"*"_zip.txt"; do
   if [ -f "$f" ]; then
-    mv "$f" "$compressed"
-    echo ::set-output name=compressed::"$compressed"
+    mv "$f" "$artifacts/compressed"
+    echo ::set-output name=compressed::"$artifacts/compressed"
   fi
 
   break
 done
 
-uncomressed="$HOME/packages/$INPUT_NAME/uncompressed"
-
 for f in "/packages/"*".txt"; do
   if [ -f "$f" ]; then
-    mv "$f" "$uncomressed"
-    echo ::set-output name=uncompressed::"$uncomressed"
+    mv "$f" "$artifacts/uncomressed"
+    echo ::set-output name=uncompressed::"$artifacts/uncomressed"
   fi
 
   break
