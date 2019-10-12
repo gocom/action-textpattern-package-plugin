01772a7497be07cf2a69b8933a0658c54c15e6f2
Jukka Svahn <void@rah.pw>
2019-10-12 20:57:51 +0300

    Fixes to the entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,19 @@
 FROM php:7.2-cli
 
+RUN apt-get update && apt-get install -y \
+  bash \
+  git
+
 # Todo replace with a modern Composer package and to allow verifying the download.
-RUN mkdir -p /compiler/ && \
+RUN ln -s /usr/local/bin/php /usr/bin/php && \
+  mkdir -p /compiler/ && \
   git clone https://github.com/gocom/MassPlugCompiler.git /compiler/bin && \
   chmod +x /compiler/bin/compile
 
 ENV PATH "$PATH:/compiler/bin"
 
 ADD entrypoint.sh /entrypoint.sh
+
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,34 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -eu
+
+if ! [ "${INPUT_NAME:-}" ]; then
+  INPUT_NAME="default"
+fi
 
 mkdir -p /packages/
 
 mkdir -p "$HOME/packages/$INPUT_NAME"
 
-compile "$GITHUB_WORKSPACE/$INPUT_SOURCE" /packages/ > /dev/null
+compile "${GITHUB_WORKSPACE:-}/${INPUT_SOURCE:-}" /packages/ > /dev/null
 
 compressed="$HOME/packages/$INPUT_NAME/compressed"
 
-uncomressed="$HOME/packages/$INPUT_NAME/uncompressed"
-
 for f in "/packages/"*"_zip.txt"; do
-  mv "$f" "$compressed"
-  echo ::set-output name=compressed::"$compressed"
+  if [ -f "$f" ]; then
+    mv "$f" "$compressed"
+    echo ::set-output name=compressed::"$compressed"
+  fi
+
   break
 done
 
+uncomressed="$HOME/packages/$INPUT_NAME/uncompressed"
+
 for f in "/packages/"*".txt"; do
-  mv "$f" "$uncomressed"
-  echo ::set-output name=uncompressed::"$uncomressed"
+  if [ -f "$f" ]; then
+    mv "$f" "$uncomressed"
+    echo ::set-output name=uncompressed::"$uncomressed"
+  fi
+
   break
 done
